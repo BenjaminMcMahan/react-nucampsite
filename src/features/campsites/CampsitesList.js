43ad405c4fc926bfa5bbe319aa158cc22839ab65
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-22 16:13:35 -0400

    Updated with a fix for horizontal scrollbar, centered page content with a Container

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,18 +1,22 @@
 import {CAMPSITES} from "../../app/shared/CAMPSITES";
-import {Col, Row} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
 
 const CampsitesList = () => {
     return (
-        <Row className='ms-auto'>
-            {CAMPSITES.map((campsite) => {
-                return (
-                    <Col md='5' className='m-4' key={campsite.id}>
-                        <CampsiteCard campsite={campsite} />
-                    </Col>
-                );
-            })}
-        </Row>
+        // Added a container here because otherwise I was getting a horizontal
+        // scrollbar...?
+        <Container>
+            <Row className='ms-auto'>
+                {CAMPSITES.map((campsite) => {
+                    return (
+                        <Col md='5' className='m-4' key={campsite.id}>
+                            <CampsiteCard campsite={campsite} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Container>
     )
 };
 
