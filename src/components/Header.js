0ad24edd735ad5a712b518b3069b5462e35ab091
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-24 12:22:21 -0400

    Added container around NavbarBrand, commented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
-import {Navbar, NavbarBrand} from "reactstrap";
+import {Navbar, NavbarBrand, Container} from "reactstrap";
 import NucampLogo from '../app/assets/img/logo.png';
 import React from "react";
 
 const Header = () => {
+    /*
+    Note - Added a container wrapping the NavbarBrand. I also added a
+    container on CampsitesList.js due to horizontal scrolling and
+    the content being shifted to the left.
+     */
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
-                <img src={NucampLogo} alt="nucamp logo"/>
-            </NavbarBrand>
+            <Container>
+                <NavbarBrand href='/'>
+                    <img src={NucampLogo} alt="nucamp logo"/>
+                </NavbarBrand>
+            </Container>
+
         </Navbar>
     )
 };
