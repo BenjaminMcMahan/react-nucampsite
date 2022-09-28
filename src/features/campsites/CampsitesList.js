1a7af8c25b70033b5e625ed5617f9e990187b518
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-28 11:31:38 -0400

    Created CampsitesSlice - this will be a single source of truth for Campsites.

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,14 +1,15 @@
-import {CAMPSITES} from "../../app/shared/CAMPSITES";
 import {Col, Container, Row} from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from "./CampsitesSlice";
 
 const CampsitesList = () => {
+    const campsites = selectAllCampsites();
     return (
         // Added a container here because otherwise I was getting a horizontal
         // scrollbar...?
         <Container>
             <Row className='ms-auto'>
-                {CAMPSITES.map((campsite) => {
+                {campsites.map((campsite) => {
                     return (
                         <Col md='5' className='m-4' key={campsite.id}>
                             <CampsiteCard campsite={campsite} />
