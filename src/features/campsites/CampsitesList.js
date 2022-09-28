3c3a624e5f7909f2d2a3e24c5a7117b098b27f31
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-28 13:26:54 -0400

    useState() exercise

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,17 +1,22 @@
-import {Col, Container, Row} from "reactstrap";
+import {Col, Row} from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
-import { selectAllCampsites } from "./CampsitesSlice";
+import {selectAllCampsites} from "./CampsitesSlice";
 
-const CampsitesList = () => {
+const CampsitesList = ({setCampsiteId}) => {
     const campsites = selectAllCampsites();
+
+
     return (
-        // Added a container here because otherwise I was getting a horizontal
-        // scrollbar...?
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
-                        <CampsiteCard campsite={campsite} />
+                    <Col
+                        md='5'
+                        className='m-4'
+                        key={campsite.id}
+                        onClick={() => setCampsiteId(campsite.id)}
+                    >
+                        <CampsiteCard campsite={campsite}/>
                     </Col>
                 );
             })}
