01d54359e91bdd03bdbe6097b96ebf4b916e20f2
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-30 11:48:59 -0400

    Implemented Breadcrumbs and SubHeader

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import {selectCampsiteById} from "../features/campsites/CampsitesSlice";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CommentsList from "../features/comments/CommentsList";
+import SubHeader from "../components/SubHeader";
 
 const CampsiteDetailPage = () => {
     // Get the ID passed from the router in App.js
@@ -12,6 +13,7 @@ const CampsiteDetailPage = () => {
 
     return (
         <Container>
+            <SubHeader current={campsite.name} detail={true}/>
             <Row>
                 <CampsiteDetail campsite={campsite}/>
                 <CommentsList campsiteId={campsiteId}/>
