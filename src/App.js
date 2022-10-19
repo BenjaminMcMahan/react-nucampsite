896f7fe665de3c984e2db339de56ee1fbfc3a96f
Benjamin McMahan <benjamin@linkedresponder.com>
2022-10-19 12:30:12 -0400

    Async Campsites - Week 5

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
@@ -8,21 +10,28 @@ import AboutPage from "./pages/AboutPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import {fetchCampsites} from "./features/campsites/campsitesSlice";
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
     return (
         <div className="App">
             <Header/>
             <Routes>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='contact' element={<ContactPage/>}/>
-                <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="contact" element={<ContactPage/>}/>
+                <Route path="directory" element={<CampsitesDirectoryPage/>}/>
                 {/* Note - this will pass the campsiteId to the CampsiteDetailPage component */}
                 <Route
-                    path='directory/:campsiteId'
+                    path="directory/:campsiteId"
                     element={<CampsiteDetailPage/>}
                 />
-                <Route path='about' element={<AboutPage/>}/>
+                <Route path="about" element={<AboutPage/>}/>
             </Routes>
             <Footer/>
         </div>
